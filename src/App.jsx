9493257771a431a96ccb28eb1cfd0458746fa530
Izma-Daudiya <168257501+Izma-Daudiya@users.noreply.github.com>
2025-05-23 10:52:15 +0000

    Sign Up and Sign In Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/register" element={<SignUp/>}></Route>
+        <Route path="/login" element={<SignIn/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
